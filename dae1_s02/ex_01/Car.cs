c5f6074e62f70929643ce2080cc222562886a24d
joyaAmora <jade.phaneuf@gmail.com>
2020-01-23 09:55:03 -0500

    semaine2

---
+++ dae1_s02/ex_01/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex_01
+{
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public string NIV { get; set; }
+        public double EngineSize { get; set; }
+
+    }
+}
