1a227135fe5f36bb27f7528c47f58aa792c231ab
joyaAmora <jade.phaneuf@gmail.com>
2020-01-27 14:45:14 -0500

    exercice 3 fini

---
+++ dae1_s02/rappel_ex_03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace rappel_ex_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb1;
+            int nb2;
+            int total;
+            string entry;
+            Console.WriteLine("Entrez 2 nombres");
+            entry = Console.ReadLine();
+            nb1 = Convert.ToInt32(entry);
+            entry = Console.ReadLine();
+            nb2 = int.Parse(entry);
+
+            Console.WriteLine("Quelle opération voulez-vous faire\nAddition A,\t Soustraction B");
+            entry = Console.ReadLine();
+            if(entry == "A")
+            {
+                total = nb1 + nb2;
+                Console.WriteLine("Voici le total: "+ total);
+            }
+            else if (entry == "B")
+            {
+                total = nb1 - nb2;
+                Console.WriteLine("Voici le total: " + total);
+            }
+            else
+            {
+                Console.WriteLine("Choix invalide");
+            }
+
+
+
+
+        }
+    }
+}
